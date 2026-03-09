ec2cf6fe58b89142492a8d6d3c3ce711316aa169
onietsabari <131362163+onietsabari@users.noreply.github.com>
2026-03-09 22:31:56 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  'https://sqyoendjnqevkqzidynv.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxeW9lbmRqbnFldmtxemlkeW52Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNzQxNTIsImV4cCI6MjA4ODY1MDE1Mn0.jJAsnRNpygXUwoV8Cq13Av31toQfxRBByCc6Q3Y30-8'
-)
